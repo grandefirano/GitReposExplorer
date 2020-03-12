8fded67eb4310ab319249ec223ac37fa21db7824
GrandeFirano <jacob.kielar@gmail.com>
2020-03-12 14:31:47 +0100

    with list item layout

--- app/src/main/java/com/grandefirano/gitreposexplorer/view/MainActivity.kt
+++ app/src/main/java/com/grandefirano/gitreposexplorer/view/MainActivity.kt
@@ -2,6 +2,9 @@ package com.grandefirano.gitreposexplorer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.grandefirano.gitreposexplorer.R
 import com.grandefirano.gitreposexplorer.contracts.MainContract
 
@@ -12,6 +15,13 @@ class MainActivity : AppCompatActivity(),
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater=menuInflater
+        inflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
 
     override fun updateList() {
         //TODO("Not yet implemented")
