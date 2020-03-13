fbd459abaa99a0fbd9a2cc29464af5d843dff298
GrandeFirano <jacob.kielar@gmail.com>
2020-03-13 18:35:46 +0100

    with detailview

--- app/src/main/java/com/grandefirano/gitreposexplorer/view/DetailsActivity.kt
+++ app/src/main/java/com/grandefirano/gitreposexplorer/view/DetailsActivity.kt
@@ -2,6 +2,8 @@ package com.grandefirano.gitreposexplorer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.grandefirano.gitreposexplorer.R
 
 class DetailsActivity : AppCompatActivity() {
@@ -9,5 +11,15 @@ class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.home->onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
