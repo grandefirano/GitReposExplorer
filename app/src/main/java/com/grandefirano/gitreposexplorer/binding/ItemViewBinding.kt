46305d80d6dd264f8640292dd3361a04884a9eb9
GrandeFirano <jacob.kielar@gmail.com>
2020-03-14 17:13:38 +0100

    with proper data updated

--- app/src/main/java/com/grandefirano/gitreposexplorer/binding/ItemViewBinding.kt
+++ app/src/main/java/com/grandefirano/gitreposexplorer/binding/ItemViewBinding.kt
@@ -1,9 +1,12 @@
 package com.grandefirano.gitreposexplorer.binding
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import com.grandefirano.gitreposexplorer.R
 import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 
 @BindingAdapter("imageUrl")
@@ -13,4 +16,15 @@ fun bindAdapterPhoto(imageView: ImageView, imageUrl: String) {
         // before it's replaced with the current image
         Picasso.with(imageView.context).load(imageUrl).into(imageView)
     }
+}
+@BindingAdapter("textDateFormat")
+fun bindAdapterDateText(textView: TextView,date:String){
+    if(date!=null){
+
+        val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        val formatter = SimpleDateFormat("dd.MM.yyyy HH:mm")
+        val output: String = formatter.format(parser.parse("2018-12-14T09:55:00Z"))
+        textView.text=output
+    }
+
 }
--- app/src/main/java/com/grandefirano/gitreposexplorer/view/DetailsActivity.kt
+++ app/src/main/java/com/grandefirano/gitreposexplorer/view/DetailsActivity.kt
@@ -1,20 +1,18 @@
 package com.grandefirano.gitreposexplorer.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Paint
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import com.grandefirano.gitreposexplorer.ExplorerApplication
 import com.grandefirano.gitreposexplorer.R
 import com.grandefirano.gitreposexplorer.api.Repo
 import com.grandefirano.gitreposexplorer.contracts.DetailsContract
 import com.grandefirano.gitreposexplorer.databinding.ActivityDetailsBinding
-import com.grandefirano.gitreposexplorer.model.ModelImpl
 import com.grandefirano.gitreposexplorer.viewmodel.DetailsViewModel
+import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity(),DetailsContract.DetailsView {
 
@@ -29,6 +27,7 @@ class DetailsActivity : AppCompatActivity(),DetailsContract.DetailsView {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp)
 
 
+        websiteTextView.setPaintFlags(websiteTextView.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
 
 
         //TODO:DOZMIANY FUUUU
